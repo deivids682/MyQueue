022e4d09326ae964a3ae954ee48228dbecc8fc2a
Deivids682 <davisiljins@inbox.lv>
2019-03-29 08:56:49 +0200

    create git repository dt_03_md

---
+++ src/Dt_str/Main.java
@@ -0,0 +1,11 @@
+package Dt_str;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public static class MyQueue {
+    }
+}
---
+++ src/Dt_str/MyQueue.java
@@ -0,0 +1,38 @@
+package Dt_str;
+
+public class MyQueue<T> {
+    private T[] queue;
+    private int front = -1;
+    private int rear = -1;
+    private final int defaultSize = 10;
+    private int elementCounter = 0;
+    private int size = defaultSize;
+
+    public MyQueue() {
+        queue = (T[]) new Object[defaultSize];
+    }
+
+    public MyQueue(int size) {
+
+        if (size <= 0) {
+            size = defaultSize * (-1);
+        }
+        queue = (T[]) new Object[size];
+    }
+
+    public boolean isEmpty() {
+        //(front == -1, rear == -1)
+        return (elementCounter == 0) ? true : false;
+    }
+
+    public boolean isFull() {
+        return (size == elementCounter)? true : false;
+    }
+
+    public int geElementCount() {
+        return elementCounter;
+    }
+
+    public void
+
+}
